1c9605df650884ff475ebfce8fd719bed93a75e5
Vladimir <vladimirtang@gmail.com>
2024-02-12 22:03:05 +0800

    made the about page look a bit better

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -4,12 +4,12 @@ const About = () =>{
 
     return (
         <>
-            <div className='flex flex-col bg-secondary h-screen w-screen'>
-                <h1 className='mt-36 mb-8'>Corn Hub</h1>
-                <h2>NPM Packages & Third party Library used</h2>
+            <div className='flex flex-col bg-secondary h-screen w-screen p-10'>
+                <h1 className='mt-36 mb-8 font-bold'>Corn Hub</h1>
+                <h2 className='font-bold'>NPM Packages & Third party Library used</h2>
                 <p className='mx-5'>Vite</p>
                 <p className='mx-5'>Tailwind</p>
-                <h3 className='mx-5'>Nodejs third-parties:</h3>
+                <h3 className='mx-5 font-bold'>Nodejs third-parties:</h3>
                 <ul className='items-start mx-8'>
                     <li>
                         cors
@@ -24,7 +24,7 @@ const About = () =>{
                         mongoose
                     </li>
                 </ul>
-                <h3 className='mx-5'>React Libraries:</h3>
+                <h3 className='mx-5 font-bold'>React Libraries:</h3>
                 <ul className='items-start mx-8'>
                     <li>
                         react-dom
@@ -37,7 +37,7 @@ const About = () =>{
                     </li>
                 </ul>
 
-                <h3 className='mx-5'>Contributors:</h3>
+                <h3 className='mx-5 font-bold'>Contributors:</h3>
                 <ul className='items-start mx-8'>
                     <li>
                         Dave Aldwin Bolima
